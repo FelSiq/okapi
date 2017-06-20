40f6017d0af916bc79402f490b53c0dfb9197c86
FelSiq <felipe.siqueira@usp.br>
2017-06-20 02:38:57 -0300

    Updated PiePlot to GeneralPlot. (working)

--- src/plotsOkapiPack/PlotPie.java
+++ src/plotsOkapiPack/PlotPie.java
@@ -22,6 +22,10 @@ public class PlotPie extends GeneralPlot {
 	// VARIABLES SECTION
 	private static BufferedImage plotImage;
 
+	private static int X_AXIS_OFFSET = GeneralPlot.getBackgroundDim()/2;	
+
+	private static int Y_AXIS_OFFSET = GeneralPlot.getBackgroundDim()/2;
+
 	private static int starter;
 	// -------------------------------------------------
 	// CLASS CONSTRUCTOR
@@ -36,10 +40,78 @@ public class PlotPie extends GeneralPlot {
 	*/
 	public static void setupPlotPie(OkapiTable<Double> dataTable, Color userColor) {
 		// Set up stuff related to PlotPie, if needed.
-		GeneralPlot.setAxis(false);
+		GeneralPlot.setAxis(true);
+
+		// Set a good visual x-axis offset 
+		GeneralPlot.setXAxisOffset(PlotPie.X_AXIS_OFFSET);
+
+		// Set a good visual y-axis offset 
+		GeneralPlot.setYAxisOffset(PlotPie.Y_AXIS_OFFSET);
 
 		// Call plot method and prepare the graphic image
 		PlotPie.plot(dataTable, userColor);
+	}
+
+	/**
+	* Create a nested (with intervals, for real numbers) frenquency vector with the user table.
+	*/
+	private static int[] createFreqVector(OkapiTable<Double> dataTable, Double xMaxValue, Double xMinValue) {
+		int[] freqVector = new int[GeneralPlot.getXInterval()];
+
+		final Double constantCoef = ((double) (GeneralPlot.getXInterval()))/(GeneralPlot.FLOAT_EQUIVALENCE + xMaxValue - xMinValue);
+		try {
+			for (int i = 0; i < dataTable.getRowNum(); i++) {
+				for (int j = 0; j < dataTable.getColNum(); j++) {
+					freqVector[((Double) ((dataTable.getElement(i, j) - xMinValue) * constantCoef)).intValue()]++; 
+				}
+			}
+		} catch (NullPointerException | IndexOutOfBoundsException | ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return freqVector;
+	}
+
+	/**
+	* Draw bars section of the barplot.
+	*/
+	private static void drawPie(Graphics2D g, int[] freqVector, Double xMinValue, Double xMaxValue, Color userColor) {
+		final Integer bgDim = GeneralPlot.getBackgroundDim();
+		final Integer subInterval = ((Double) ((xMaxValue - xMinValue)/GeneralPlot.getXInterval())).intValue();
+
+		final int halfRadius = bgDim/2;
+
+		g.setColor(Color.BLACK);
+		g.drawOval(0, 0, bgDim, bgDim);
+
+		int freqSum = 0;
+		for (int i = 0; i < freqVector.length; i++) {
+			freqSum += freqVector[i];
+		}
+
+		int starter = 0;
+		Integer currentInterval = 0;
+		for(int i = 0; i < freqVector.length; i++) {
+			g.fillRect(5, 5 + i*26, 20, 20);
+			g.drawString( "[" + currentInterval + ", " + (currentInterval + subInterval) + ")", 30, 5 + 13 + i*26);
+			currentInterval += subInterval;
+
+			Random rand = new Random();
+			Color c = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+			g.setColor(c);
+
+			int thetaval = ((360 * freqVector[i])/freqSum);
+			g.fillArc(0, 0, bgDim, bgDim, starter, thetaval);
+
+			starter += thetaval;
+		}
+	}
+
+	/**
+	* Set up custom maximum y-axis limits, based on the built frequency vector.
+	*/
+	private static void setCorrectYLimMax() {
+		GeneralPlot.setYLimits(GeneralPlot.getXLimMin(), GeneralPlot.getXLimMax());
 	}
 
 	/**
@@ -48,39 +120,42 @@ public class PlotPie extends GeneralPlot {
 	private static void plot(OkapiTable<Double> dataTable, Color userColor) {
 		// Get the plotting space dimensions
 		Integer bgDim = GeneralPlot.getBackgroundDim();
-		starter = 0;
-		
+
 		// Create the basis image for this plot
 		PlotPie.plotImage = new BufferedImage(
 			bgDim, bgDim, BufferedImage.TYPE_4BYTE_ABGR);
 
 		// Creates the basis drawer of this plot
-		Graphics2D g = (Graphics2D) plotImage.getGraphics();
-		JPanel panel = new JPanel();
-		
-		g.drawOval(bgDim/2, bgDim/2, 300, 300);
-		
-		for(int i=0;i<dataTable.getRowNum();i++){
-			Random rand = new Random();
-			Color c = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255),
-					rand.nextInt(255));
-			JLabel lab = new JLabel(dataTable.getRowName(i),JLabel.RIGHT);
-			panel.add(lab);
-			
-			g.setColor(c);
-			g.drawRect(5, 10, 20, 20);
-			g.fillArc(bgDim/2, bgDim/2, 300, 300, starter, 
-				(int) (starter+(360/dataTable.getElement(i, 0))));
-			starter +=  360/dataTable.getRowNum();
-		}
-		
-		panel.setVisible(true);
-		// INSTRUCTIONS: 
-		// 1. Just draw on g (g.drawLine, g.drawOval, g.drawRect...)
-		// And the magic will just happens. Trust me.
-		// 2. If you want to plot a (x, y) dot, then always draw
-		// on the (GeneralPlot.getXPosition(x), GeneralPlot.getYPosition(y)) 
-		// This will convert the (x, y) value to the correct (x', y') screen position.
+		Graphics2D g = (Graphics2D) PlotPie.plotImage.getGraphics();
 
+		// Auxiliary variables, to set x-axis and y-axis limits adequately.
+		Double xMaxValue = Double.MIN_VALUE, xMinValue = Double.MAX_VALUE;
+
+		// Get the min and max of the table.
+		try {
+			Double dummy = 0.0;
+			for (int i = 0; i < dataTable.getRowNum(); i++) {
+				for (int j = 0; j < dataTable.getColNum(); j++) {
+					dummy = dataTable.getElement(i, j);
+					xMaxValue = (xMaxValue < dummy ? dummy : xMaxValue);
+					xMinValue = (xMinValue > dummy ? dummy : xMinValue);
+				}
+			}
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+
+		// Create a frequency vector, for each nest
+		int[] freqVector = createFreqVector(dataTable, xMaxValue, xMinValue);
+		
+		// Set up a good stroke (just for aesthetics)
+		BasicStroke strokeStyle = new BasicStroke(2);
+		g.setStroke(strokeStyle);
+
+		// 
+		PlotPie.setCorrectYLimMax();
+
+		// Draw bars
+		PlotPie.drawPie(g, freqVector, xMinValue, xMaxValue, userColor);
 	}
 }
