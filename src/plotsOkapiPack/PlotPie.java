e8c59904df69c2b63603ddef60f8284f03aaff37
MartheusB <bsmatheus2016@gmail.com>
2017-06-19 23:28:38 -0300

    Add files via upload

--- src/plotsOkapiPack/PlotPie.java
+++ src/plotsOkapiPack/PlotPie.java
@@ -7,6 +7,11 @@ import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import java.awt.Color;
 import java.util.List;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 // -------------------------------------------------
 
 /**
@@ -17,6 +22,7 @@ public class PlotPie extends GeneralPlot {
 	// VARIABLES SECTION
 	private static BufferedImage plotImage;
 
+	private static int starter;
 	// -------------------------------------------------
 	// CLASS CONSTRUCTOR
 	public PlotPie() {
@@ -42,14 +48,33 @@ public class PlotPie extends GeneralPlot {
 	private static void plot(OkapiTable<Double> dataTable, Color userColor) {
 		// Get the plotting space dimensions
 		Integer bgDim = GeneralPlot.getBackgroundDim();
-
+		starter = 0;
+		
 		// Create the basis image for this plot
 		PlotPie.plotImage = new BufferedImage(
 			bgDim, bgDim, BufferedImage.TYPE_4BYTE_ABGR);
 
 		// Creates the basis drawer of this plot
 		Graphics2D g = (Graphics2D) plotImage.getGraphics();
-
+		JPanel panel = new JPanel();
+		
+		g.drawOval(bgDim/2, bgDim/2, 300, 300);
+		
+		for(int i=0;i<dataTable.getRowNum();i++){
+			Random rand = new Random();
+			Color c = new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255),
+					rand.nextInt(255));
+			JLabel lab = new JLabel(dataTable.getRowName(i),JLabel.RIGHT);
+			panel.add(lab);
+			
+			g.setColor(c);
+			g.drawRect(5, 10, 20, 20);
+			g.fillArc(bgDim/2, bgDim/2, 300, 300, starter, 
+				(int) (starter+(360/dataTable.getElement(i, 0))));
+			starter +=  360/dataTable.getRowNum();
+		}
+		
+		panel.setVisible(true);
 		// INSTRUCTIONS: 
 		// 1. Just draw on g (g.drawLine, g.drawOval, g.drawRect...)
 		// And the magic will just happens. Trust me.
